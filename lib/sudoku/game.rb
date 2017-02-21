041bebc1aa7659d36edbbab32c52b258e46d0d1d
unmanbearpig <unmanbearpig@gmail.com>
2017-02-21 07:57:36 +0300

    Add blank game classes

---
+++ lib/sudoku/game.rb
@@ -0,0 +1,10 @@
+module Sudoku
+  class Game
+    def load_board(*board_values)
+      @board_values = board_values
+    end
+
+    def solve
+    end
+  end
+end
---
+++ lib/sudoku/game_board.rb
@@ -0,0 +1,7 @@
+module Sudoku
+  class GameBoard
+    def initialize(*board_values)
+      @board_values = board_values
+    end
+  end
+end
