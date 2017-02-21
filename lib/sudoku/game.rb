a57db0ec238e3031bdc2fcd2c588d325a0c79d92
unmanbearpig <unmanbearpig@gmail.com>
2017-02-21 08:02:41 +0300

    Use a GameBoard in Game

--- lib/sudoku/game.rb
+++ lib/sudoku/game.rb
@@ -1,7 +1,9 @@
+require 'sudoku/game_board'
+
 module Sudoku
   class Game
     def load_board(*board_values)
-      @board_values = board_values
+      @board = GameBoard.new(board_values)
     end
 
     def solve
