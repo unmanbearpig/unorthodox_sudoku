304c5b862297e79dfc1b60f91338f6d0b3c5608b
unmanbearpig <unmanbearpig@gmail.com>
2017-02-21 07:32:25 +0300

    Initial commit (bundle gem)

---
+++ lib/sudoku.rb
@@ -0,0 +1,5 @@
+require "sudoku/version"
+
+module Sudoku
+  # Your code goes here...
+end
---
+++ spec/sudoku_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Sudoku do
+  it "has a version number" do
+    expect(Sudoku::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
