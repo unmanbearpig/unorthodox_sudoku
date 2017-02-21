fc726791ce470e4b1c72236d1d3664817bd49757
unmanbearpig <unmanbearpig@gmail.com>
2017-02-22 00:52:57 +0300

    Extract a solving strategy

--- lib/sudoku/game.rb
+++ lib/sudoku/game.rb
@@ -6,16 +6,52 @@ module Sudoku
       @board = GameBoard.new(*board_values)
     end
 
-    def solve
-      pboard = PossibilityBoard.new(@board.possible_values)
-      solve_possibility_board(pboard).to_board
+    def strategy
+      DefaultSolvingStrategy.new(PossibilityBoard.new(@board.possible_values))
     end
 
-    def make_board_permutations(board)
-      best = board.best_permutations_to_try
+    def solve
+      strategy.solve_possibility_board.to_board
+    end
+  end
+
+  class DefaultSolvingStrategy
+    attr_reader :board
+    def initialize(board)
+      @board = board
+    end
+
+    def unknown_values
+      board.unknown_values
+    end
+
+    def most_common_unknown_values
+      @most_common_unknown_values ||=
+        unknown_values.flat_map(&:value).flat_map(&:to_a).sort.reduce(Hash.new(0)) do |acc, value|
+        acc[value] += 1
+        acc
+      end
+    end
+
+    def best_combinations_to_try
+      return nil unless unknown_values.any?
+
+      smallest_size = unknown_values.first.value.size
+
+      unknown_values.select { |v| v.value.size == smallest_size }
+        .sort_by { |v| v.value.map { |num| most_common_unknown_values[num] } }
+    end
+
+    def best_permutations_to_try
+      @best_permutations_to_try ||= best_combinations_to_try&.permutation
+    end
+
+    def board_permutations
+      return @board_permutations if @board_permutations
+      best = best_permutations_to_try
       return nil unless best
 
-      best.lazy.flat_map do |combinations|
+      @board_permutations = best.lazy.flat_map do |combinations|
         combinations.flat_map do |possibility_set|
           coordinates = possibility_set.coordinates
           possibility_set.value.map { |value| board.set(coordinates, value) }
@@ -23,13 +59,12 @@ module Sudoku
       end
     end
 
-    def solve_possibility_board(board)
+    def solve_possibility_board
       return board if board.solved?
       return nil if board.completed?
 
-      boards = make_board_permutations(board)
-      boards.each do |new_board|
-        result = solve_possibility_board(new_board)
+      board_permutations.each do |new_board|
+        result = self.class.new(new_board).solve_possibility_board
         return result if result
       end
 
