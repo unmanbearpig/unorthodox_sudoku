dcf5ec4965e21a253f7cace9a2cfd603adf49da3
unmanbearpig <unmanbearpig@gmail.com>
2017-02-22 09:47:42 +0300

    Update GameBoard spec

--- spec/sudoku/game_board_spec.rb
+++ spec/sudoku/game_board_spec.rb
@@ -4,21 +4,8 @@ require 'sudoku/game_board'
 
 module Sudoku
   describe GameBoard do
-    let(:values) do
-      [0, 0, 8, 3, 4, 2, 9, 0, 0,
-       0, 0, 9, 0, 0, 0, 7, 0, 0,
-       4, 0, 0, 0, 0, 0, 0, 0, 3,
-       0, 0, 6, 4, 7, 3, 2, 0, 0,
-       0, 3, 0, 0, 0, 0, 0, 1, 0,
-       0, 0, 2, 8, 5, 1, 6, 0, 0,
-       7, 0, 0, 0, 0, 0, 0, 0, 8,
-       0, 0, 4, 0, 0, 0, 1, 0, 0,
-       0, 0, 3, 6, 9, 7, 5, 0, 0]
-    end
-    subject(:board) { GameBoard.new(*values) }
-
-    describe "#possible_values" do
-      it "maps each value on the board to a set of possible values in that position" do
+    describe "#==" do
+      it "is equal to the board with same values" do
         values = [0, 7, 8, 3, 4, 2, 9, 5, 0,
                   3, 2, 9, 1, 8, 5, 7, 6, 4,
                   4, 5, 0, 7, 6, 9, 8, 2, 3,
@@ -29,8 +16,7 @@ module Sudoku
                   2, 9, 4, 5, 3, 8, 1, 7, 6,
                   0, 8, 3, 6, 9, 7, 5, 4, 2]
 
-        board = GameBoard.new(*values)
-        expect(board.possible_values[Coordinates.new(0, 0)]).to eq(Set.new([6, 1]))
+        expect(GameBoard.new(*values)).to eq(GameBoard.new(*values))
       end
     end
   end
